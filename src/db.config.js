3cc82184ca5fdb5bb31174b2894a161be9c6e455
AllanMuhari <allanmuhari087@gmail.com>
2024-06-23 22:08:49 +0300

    add readme , .env file

--- src/db.config.js
+++ src/db.config.js
@@ -1,13 +1,15 @@
 import pkg from "pg";
-
+import { config } from "dotenv";
 const { Pool } = pkg;
 
+config();
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "smart_shop",
-  password: "9828",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 export default pool;
